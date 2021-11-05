8173d38f75a2e798c51f15693d81d8a704c06eb5
Tim Harpe <tim.harpe@t-online.de>
2021-11-05 18:38:00 +0100

    feat: add xlb

--- main.tf
+++ main.tf
@@ -30,3 +30,52 @@ resource "google_compute_instance_group" "this" {
     port = "80"
   }
 }
+
+resource "google_compute_health_check" "this" {
+  name               = "check-backend"
+  check_interval_sec = 1
+  timeout_sec        = 1
+  tcp_health_check {
+    port = "80"
+  }
+}
+
+resource "google_compute_backend_service" "this" {
+  name                  = "l7-xlb-backend-service"
+  provider              = google-beta
+  protocol              = "HTTP"
+  port_name             = "http"
+  load_balancing_scheme = "EXTERNAL"
+  health_checks         = [google_compute_health_check.this.id]
+  backend {
+    group           = google_compute_instance_group.this.id
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+}
+
+resource "google_compute_global_address" "this" {
+  name = "l7-xlb-static-ip"
+}
+
+resource "google_compute_global_forwarding_rule" "this" {
+  name                  = "l7-xlb-forwarding-rule"
+  provider              = google
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.this.id
+  ip_address            = google_compute_global_address.this.id
+}
+
+resource "google_compute_target_http_proxy" "this" {
+  name     = "l7-xlb-target-http-proxy"
+  provider = google
+  url_map  = google_compute_url_map.this.id
+}
+
+resource "google_compute_url_map" "this" {
+  name            = "l7-xlb-url-map"
+  provider        = google
+  default_service = google_compute_backend_service.this.id
+}
