e89aaa2d1d4361db53b3da4128ab9cf2fbac885d
Tim Harpe <tim.harpe@t-online.de>
2021-11-05 18:15:38 +0100

    Initial code commit

---
+++ backend.tf
@@ -0,0 +1,6 @@
+terraform {
+  backend "gcs" {
+    bucket = "12435657ioter345677iuztrew"
+    prefix = "terraform/"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  #credentials = file("../key.json")
+  project = "playground-s-11-b0962ad6"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "google_compute_instance" "this" {
+  name                    = "debian09"
+  machine_type            = "e2-small"
+  metadata_startup_script = "apt update && apt -y install apache2 && echo '<html><body><p>Linux startup script added directly.</p></body></html>' > /var/www/html/index.html"
+  tags                    = ["http-server", "https-server"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance_group" "this" {
+  name      = "instance-group"
+  instances = [google_compute_instance.this.id]
+  named_port {
+    name = "http"
+    port = "8080"
+  }
+}
